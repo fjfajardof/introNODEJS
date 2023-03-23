a6639626bee6d12cf27c8bd1bd22b2146059304e
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-23 18:13:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const sumar= require("./sumar");
+let suma=sumar(4,5);
+console.log(suma);
---
+++ testsumar.test.js
@@ -0,0 +1,8 @@
+const sumar= require("./sumar");
+test("Probas suma de dos enteros",()=>{
+
+expect(sumar(3,5)).toBe(8);
+
+}
+
+)
